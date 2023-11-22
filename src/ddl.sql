e9640ea1e2b3d75c5a31d4051a91ae71d2a1b3bf
Thomas REMY <contact.t.remy777@gmail.com>
2023-11-22 20:14:38 +0100

    Refactor database schema and table definitions
    - Updated table names to be more descriptive
    - Renamed columns for clarity
    - Changed data types for gender, contract type, school level, staff function, and room type to VARCHAR
    - Added CHECK constraints for valid values in gender, contract type, school level, staff function, and room type columns

--- src/ddl.sql
+++ src/ddl.sql
@@ -1,19 +1,18 @@
 DROP TABLE IF EXISTS person, "user", parent, staff, internal_staff,
     external_staff, staff_presence, building, room, activity, event,
-    room_logs, building_logs, participate, subscribe, child, organize, propose CASCADE;
+    room_log, building_log, participate, subscribe, child, organize, propose CASCADE;
 
 DROP TYPE IF EXISTS gender, contract_type, staff_function, room_type, school_level;
 
 CREATE TYPE gender AS ENUM (
-    'MALE',
-    'FEMALE'
+    'MALE', 'FEMALE'
 );
 
 CREATE TABLE IF NOT EXISTS person(
     person_id SERIAL,
     person_fname VARCHAR(50) NOT NULL CHECK ( person_fname ~ '^[[:upper:]][[:lower:]]+([[:space:]][[:upper:]])?([[:space:]]?[[:upper:]][[:lower:]]+)*$' ),
     person_lname VARCHAR(50) NOT NULL CHECK ( person_lname ~ '^([[:upper:]]+[[:space:]]?)+$' ),
-    person_gender gender NOT NULL,
+    person_gender VARCHAR(6) NOT NULL CHECK ( person_gender IN ( 'MALE', 'FEMALE' ) ),
     person_birth_date DATE NOT NULL CHECK (person_birth_date < CURRENT_DATE AND person_birth_date > CURRENT_DATE - INTERVAL '150 years'),
     PRIMARY KEY (person_id)
 );
@@ -47,7 +46,7 @@ CREATE TYPE school_level AS ENUM (
 
 CREATE TABLE IF NOT EXISTS child(
     child_id INTEGER,
-    child_school_level school_level NULL,
+    child_school_level VARCHAR(6) NULL CHECK ( child_school_level IN ( 'YEAR1', 'YEAR2', 'YEAR3', 'YEAR4', 'YEAR5', 'YEAR6', 'YEAR7', 'YEAR8', 'YEAR9', 'YEAR10', 'YEAR11', 'YEAR12' ) ),
     parent_id INTEGER NOT NULL,
     PRIMARY KEY (child_id),
     FOREIGN KEY (child_id) REFERENCES person(person_id),
@@ -62,7 +61,7 @@ CREATE TABLE IF NOT EXISTS staff(
     staff_id INTEGER,
     staff_email VARCHAR(320) NOT NULL UNIQUE CHECK ( staff_email ~ '^[\w!#$%&''/*+=?`{|}~^-]+(?:\.[\w!#$%&''/*+=?`{|}~^-]+)*@(?:[a-z0-9-]+\.)+[a-z]{2,6}$' ),
     staff_phone CHAR(10) NOT NULL UNIQUE CHECK ( staff_phone ~ '^0[[:digit:]]{9}$' ),
-    staff_contract_type contract_type NOT NULL,
+    staff_contract_type VARCHAR(9) NOT NULL CHECK ( staff_contract_type IN ( 'PERMANENT', 'TEMPORARY', 'INTERIM', 'SERVICE' ) ),
     PRIMARY KEY (staff_id),
     FOREIGN KEY (staff_id) REFERENCES "user"(user_id)
 );
@@ -82,7 +81,7 @@ CREATE TYPE staff_function AS ENUM (
 CREATE TABLE IF NOT EXISTS internal_staff(
     int_staff_id INTEGER,
     int_staff_hr_number INTEGER NOT NULL UNIQUE CHECK ( int_staff_hr_number > 0 ),
-    int_staff_function staff_function NOT NULL,
+    int_staff_function VARCHAR(9) NOT NULL CHECK ( int_staff_function IN ( 'EXECUTIVE', 'SECRETARY', 'EMPLOYEE' ) ),
     address_street_number INTEGER NOT NULL CHECK ( address_street_number > 0 AND address_street_number < 10000 ),
     address_street_name VARCHAR(50) NOT NULL,
     address_zip_code CHAR(5) NOT NULL CHECK ( address_zip_code ~ '^[0-9]{5}$'),
@@ -121,7 +120,7 @@ CREATE TABLE IF NOT EXISTS room(
     room_name VARCHAR(20) NOT NULL,
     room_floor INTEGER NOT NULL CHECK (room_floor >= 0 AND room_floor < 500),
     room_number INTEGER NOT NULL CHECK (room_number >=0),
-    room_type room_type NOT NULL,
+    room_type VARCHAR(12) NOT NULL CHECK ( room_type IN ( 'AMPHITHEATER', 'ROOM', 'WORKSHOP' ) ),
     room_capacity INTEGER NOT NULL CHECK (room_capacity >= 1 AND room_capacity < 50000),
     building_id INTEGER NOT NULL,
     PRIMARY KEY (room_id),
